9d3321e396b446a550c2787e71124efca22f6aca
Gehana Spoorty Kodeboyina <s2581051@frodo.inf.ed.ac.uk>
2026-02-16 22:00:36 +0000

    new

--- src/pf_localisation/pf_localisation/pf.py
+++ src/pf_localisation/pf_localisation/pf.py
@@ -25,7 +25,7 @@ class PFLocaliser(PFLocaliserBase):
         # ----- Sensor model parameters
         self.NUMBER_PREDICTED_READINGS = 20
 
-        # ----- Adaptive MCL parameters
+        # Adaptive MCL parameters
         self.MIN_PARTICLES = 300
         self.MAX_PARTICLES = 2000
 
@@ -174,70 +174,6 @@ class PFLocaliser(PFLocaliserBase):
         
         return M_x
 
-    # def update_particle_cloud(self, scan):
-        
-    #     S = self.particlecloud.poses
-    #     M = len(S)
-        
-    #     RESAMPLE_NOISE_X = 0.02      
-    #     RESAMPLE_NOISE_Y = 0.02 
-    #     RESAMPLE_NOISE_THETA = 0.02
-
-    #     num_random_particles = int(self.RANDOM_PARTICLE_RATIO * M)
-    #     num_resampled_particles = M - num_random_particles
-        
-    #     weights = []
-    #     for pose in S:
-    #         w_i = self.sensor_model.get_weight(scan, pose)
-    #         weights.append(w_i)
-        
-    #     total_weight = sum(weights)
-
-    #     if total_weight == 0:
-    #         weights = [1.0] * M
-    #         total_weight = M
-        
-    #     normalized_weights = [w / total_weight for w in weights]
-
-    #     self._weights_before_resample = normalized_weights
-    #     self._particles_before_resample = [self._copy_pose(pose) for pose in S]
-        
-    #     c = [normalized_weights[0]]
-    #     for i in range(1, M):
-    #         c_i = c[i-1] + normalized_weights[i]
-    #         c.append(c_i)
-        
-    #     S_prime = []
-    #     u = random.uniform(0, 1.0 / num_resampled_particles)
-    #     i = 0
-        
-    #     for j in range(num_resampled_particles):
-
-    #         while u > c[i]:
-    #             i = i + 1
-            
-    #         selected = S[i]
-    #         new_particle = Pose()
-            
-    #         new_particle.position.x = selected.position.x + random.gauss(0, RESAMPLE_NOISE_X)
-    #         new_particle.position.y = selected.position.y + random.gauss(0, RESAMPLE_NOISE_Y)
-    #         new_particle.position.z = 0.0
-            
-    #         old_heading = getHeading(selected.orientation)
-    #         new_heading = old_heading + random.gauss(0, RESAMPLE_NOISE_THETA)
-    #         new_particle.orientation = rotateQuaternion(Quaternion(w=1.0), new_heading)
-            
-    #         S_prime.append(new_particle)
-            
-    #         u = u + 1.0 / num_resampled_particles
-
-    
-    #     for j in range(num_random_particles):
-    #         random_particle = self._create_random_particle()
-    #         S_prime.append(random_particle)
-        
-    #     self.particlecloud.poses = S_prime
-
     def update_particle_cloud(self, scan):
         
         S = self.particlecloud.poses
@@ -348,79 +284,6 @@ class PFLocaliser(PFLocaliserBase):
         self.particlecloud.poses = S_prime
 
 
-
-    # def estimate_pose(self):
-    #     """
-    #     This should calculate and return an updated robot pose estimate based
-    #     on the particle cloud (self.particlecloud).
-        
-    #     Create new estimated pose, given particle cloud
-    #     E.g. just average the location and orientation values of each of
-    #     the particles and return this.
-        
-    #     Better approximations could be made by doing some simple clustering,
-    #     e.g. taking the average location of half the particles after 
-    #     throwing away any which are outliers
-
-    #     :Return:
-    #         | (geometry_msgs.msg.Pose) robot's estimated pose.
-    #     """
-       
-    #     S = self.particlecloud.poses
-
-    #     weights = self._weights_before_resample
-    #     max_weight_idx = weights.index(max(weights))
-    #     best_particle = S[max_weight_idx]
-
-    #     curr_x = best_particle.position.x
-    #     curr_y = best_particle.position.y
-    #     curr_theta = getHeading(best_particle.orientation)
-
-    #     radius = 0.7 
-    #     iterations = 5
-        
-    #     for _ in range(iterations):
-    #         weighted_x_sum = 0.0
-    #         weighted_y_sum = 0.0
-    #         weighted_sin_sum = 0.0
-    #         weighted_cos_sum = 0.0
-    #         total_influence = 0.0
-
-    #         for i, p in enumerate(S):
-                
-    #             dx = p.position.x - curr_x
-    #             dy = p.position.y - curr_y
-    #             dist_sq = dx**2 + dy**2
-
-                
-    #             if dist_sq < radius**2:
-                    
-    #                 influence = weights[i] * math.exp(-dist_sq / (2 * (radius/2)**2))
-                    
-    #                 weighted_x_sum += p.position.x * influence
-    #                 weighted_y_sum += p.position.y * influence
-                    
-    #                 p_theta = getHeading(p.orientation)
-    #                 weighted_sin_sum += math.sin(p_theta) * influence
-    #                 weighted_cos_sum += math.cos(p_theta) * influence
-                    
-    #                 total_influence += influence
-
-    #         if total_influence > 0:
-    #             curr_x = weighted_x_sum / total_influence
-    #             curr_y = weighted_y_sum / total_influence
-    #             curr_theta = math.atan2(weighted_sin_sum, weighted_cos_sum)
-    #         else:
-    #             break 
-
-    #     estimated_pose = Pose()
-    #     estimated_pose.position.x = curr_x
-    #     estimated_pose.position.y = curr_y
-    #     estimated_pose.position.z = 0.0
-    #     estimated_pose.orientation = rotateQuaternion(Quaternion(w=1.0), curr_theta)
-
-    #     return estimated_pose
-
     def estimate_pose(self):
         """
         This should calculate and return an updated robot pose estimate based
@@ -438,7 +301,6 @@ class PFLocaliser(PFLocaliserBase):
             | (geometry_msgs.msg.Pose) robot's estimated pose.
         """
        
-        # Use pre-resample particles and weights for consistent estimation
         if len(self._particles_before_resample) == 0:
             S = self.particlecloud.poses
             weights = [1.0 / len(S)] * len(S)
